2cd20fe9051aa2f11cdfcf0d3a07147a0c8c4771
student-a210 <student-a210@PC-A210-7>
2024-04-26 14:21:08 +0500

    Выполненная практическая работа на оценку удовлетворительно. Драчёв

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,13 +16,29 @@ using System.Windows.Shapes;
 namespace PR29
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            OpenPages(new Pages.Clubs.Main());
+        }
+        public void OpenPages(Page Page)
+        {
+            frame.Navigate(Page);
+        }
+        private void Clubs(object sender, RoutedEventArgs e)
+        {
+            OpenPages(new Pages.Clubs.Main());
+        }
+        private void Users(object sender, RoutedEventArgs e)
+        {
+            OpenPages(new Pages.Users.Main());
+
         }
     }
 }
